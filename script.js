f6aaf1c21715bd519a623848e0e58535cad6aca9
owentr1369 <tamvaa1306@gmail.com>
2022-06-13 14:19:34 +0700

    scrollTop to hide thumb cd

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import {songs} from './store/music/music.js'
+import { songs } from "./store/music/music.js";
 
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 const app = {
-    render: function(){
-        const htmls = songs.map(song=> {
-    return   `     <div class="song">
+  render: function () {
+    const htmls = songs.map((song) => {
+      return `     <div class="song">
         <div
           class="thumb"
           style="background-image: url('${song.image}')"
@@ -18,12 +18,25 @@ const app = {
         <div class="option">
           <i class="fas fa-ellipsis-h"></i>
         </div>
-      </div>`
-        })
-        $('.playlist').innerHTML = htmls.join('')
-    },
-    start: function(){
-        this.render()
-    }
-}
-app.start()
+      </div>`;
+    });
+    $(".playlist").innerHTML = htmls.join("");
+  },
+  handleEvents: function () {
+    const cd = $(".cd");
+    const cdWidtd = cd.offsetWidth;
+
+    document.onscroll = function () {
+      const scrollTop = window.screenY || document.documentElement.scrollTop;
+      const newCdWidth = cdWidtd - scrollTop;
+
+      cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0 + "px";
+      cd.style.opacity = newCdWidth / cdWidtd;
+    };
+  },
+  start: function () {
+    this.handleEvents();
+    this.render();
+  },
+};
+app.start();
