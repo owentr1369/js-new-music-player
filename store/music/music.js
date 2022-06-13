4271a231f70eb95ab706db3fae92abbca820a68b
owentr1369 <tamvaa1306@gmail.com>
2022-06-13 13:30:03 +0700

    music data

--- store/music/music.js
+++ store/music/music.js
@@ -1,16 +1,64 @@
 export const app = {
   songs: [
     {
-      name: "",
-      singer: "",
-      path: "",
-      image: "",
+      name: "okokok",
+      singer: "Low G",
+      path: "./mp3/okeokeoke - Low G.mp3",
+      image: "./thumbnail/okokok.jpg",
     },
     {
-      name: "",
-      singer: "",
-      path: "",
-      image: "",
+      name: "Tam Giác",
+      singer: "Phan Anh x Low G",
+      path: "./mp3/Tam Giác - Anh Phan ft Low G.mp3",
+      image: "./thumbnail/tamgiac.jpg",
+    },
+    {
+      name: "Ghệ Đẹp",
+      singer: "Cain x LCKing",
+      path: "./mp3/Cain - Ghệ Đẹp ft LCKing.mp3",
+      image: "./thumbnail/ghedep.jpg",
+    },
+    {
+      name: "Duyên Âm",
+      singer: "Hoàng Thùy Linh",
+      path: "./mp3/Duyên Âm - Hoàng Thùy Linh.mp3",
+      image: "./thumbnail/duyenam.jpg",
+    },
+    {
+      name: "Mời Trầu",
+      singer: "Masew",
+      path: "./mp3/Mời Trầu - Masew.mp3",
+      image: "./thumbnail/moitrau.jpg",
+    },
+    {
+      name: "Người Đi Bao",
+      singer: "TLinh x Low G",
+      path: "./mp3/Người Đi Bao - TLinh x Low G.mp3",
+      image: "./thumbnail/nguoidibao.jpg",
+    },
+    {
+      name: "Nhất Thân",
+      singer: "Masew",
+      path: "./mp3/Nhất Thân - Masew x Khoi Vu.mp3",
+      image: "./thumbnail/nhatthan.jpg",
+    },
+    {
+      name: "Ôm Em Lần Cuối",
+      singer: "Nit x SingCukCak",
+      path: "./mp3/Ôm Em Lần Cuối - Nit ft SingCukak.mp3",
+      image: "./thumbnail/omemlancuoi.jpg",
+    },
+    {
+      name: "Simple Cypher",
+      singer: "Low G",
+      path: "./mp3/Simple Cypher - Low G.mp3",
+      image: "./thumbnail/simplecypher.jpg",
+    },
+    {
+      name: "Túy Âm",
+      singer: "Masew",
+      path: "./mp3/Túy Âm - Xesi x Masew x Nhatnguyen.mp3",
+      image: "./thumbnail/tuyam.jpg",
     },
   ],
 };
